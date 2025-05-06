724e45d04fb5519bb3bd84c2db7a296b0058cb56
Anarquia122 <italogusto823@gmail.com>
2025-05-05 22:19:23 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/italo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/italo/workshopmongo/config/Instantiation.java
@@ -1,12 +1,15 @@
 package com.italo.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.italo.workshopmongo.domain.Post;
 import com.italo.workshopmongo.domain.User;
+import com.italo.workshopmongo.repositiories.PostRepository;
 import com.italo.workshopmongo.repositiories.UserRepository;
 
 @Configuration
@@ -15,16 +18,26 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepo;
 	
+	@Autowired
+	private PostRepository postRepo;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		userRepo.deleteAll();
+		postRepo.deleteAll();
 		
 		User grimmjow = new User(null, "Grimmjow", "grim@gmail.com");
 		User ulquiorra = new User(null, "Ulquiorra", "ulq@gmail.com");
 		User stark = new User(null, "Stark", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Saindo do Hueco mundo", "Ainzen mandou buscar a Orihime.", ulquiorra);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Partiu invadir Karakura", "Tô indo atrás daquele shinigami substituto", grimmjow);
+
 		userRepo.saveAll(Arrays.asList(grimmjow, ulquiorra, stark));
+		postRepo.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
